7a7072d2c4aecf734cdaf27f0f390b417ea66186
Mr. Lange <blange@gulllakecs.org>
2023-02-09 07:46:01 -0500

    Added Hello World and Ninety Degree Functions - Stable

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,3 +1,38 @@
 //Programmer: Mr. Lange
 //Date: 2.6.2023
 //Project: Sphero Bolt
+
+
+/**
+ * Hello World Practice Program
+ * LED turn to Blue
+ * Forward for 2 seconds at a speed of 60
+*/
+async function helloWorld() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+}
+
+/**
+ * ninetyDegree function goes forward for a specified amount of time and speed
+ * pausing, turning 90degrees clockwise then moving forward for a specific amount
+ * of speed and time
+*/
+async function ninetyDegree() {
+	await roll((getHeading() + 0), 60, 4); // Heading 0 degrees at 60 speed for 4 seconds
+	await delay(1);
+	await roll((getHeading() + 90), 60, 4); // heading 90 degrees at 60 speed for 4 seconds
+}
+
+/**
+ * startProgram function sends the program to your Sphero Bolt
+*/
+async function startProgram() {
+	await helloWorld() //<--calling the Helloi WOrld Function
+	await ninetyDegree()  //<--calling the ninetyDegree function to run it
+}
+
+
